b1670a439d0b78d5ded04417d93e04171c84b118
whossname <tyson.buzza@gmail.com>
2019-11-03 15:59:50 +0800

    hehe

--- lib/travis/build/bash/travis_setup_mssql_server.bash
+++ lib/travis/build/bash/travis_setup_mssql_server.bash
@@ -16,6 +16,7 @@ travis_setup_mssql_server() {
     # install libjemalloc
     wget -q http://ftp.osuosl.org/pub/ubuntu/pool/universe/j/jemalloc/libjemalloc1_3.6.0-11_amd64.deb
     dpkg -i libjemalloc1_3.6.0-11_amd64.deb
+    rm libjemalloc1_3.6.0-11_amd64.deb
     ;;
   *)
     echo -e "${ANSI_RED}Unrecognized operating system.${ANSI_CLEAR}"
